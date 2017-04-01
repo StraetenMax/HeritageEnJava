890325ad015c03fba5504439342ff7cf514e245b
StraetenMax <maxstraeten@yahoo.com>
2017-04-01 15:19:16 +0200

    création de la classe Triangle

---
+++ src/Forme.java
@@ -0,0 +1,9 @@
+
+public class Forme {
+	//attribut
+	protected int cote = 4;
+	//methode
+	public int aire(){
+		return cote*cote;
+	}
+}
--- src/TestHeritage.java
+++ src/TestHeritage.java
@@ -5,6 +5,7 @@ public class TestHeritage {
 		Enfant enfant1 = new Enfant();
 		System.out.println(enfant1.nombreDeTetes());
 		System.out.println(new Enfant().nombreDeTetes());
+		System.out.println(new Carre().aire());
 	}
 
 }
