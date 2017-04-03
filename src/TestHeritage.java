ea7a7076ded32bd1edfd3eac0cebe0f92c070d80
StraetenMax <maxstraeten@yahoo.com>
2017-04-03 06:54:22 +0200

    covariance de variables

--- src/TestHeritage.java
+++ src/TestHeritage.java
@@ -11,6 +11,24 @@ public class TestHeritage {
 		System.out.println(new Triangle().aire1());
 		System.out.println(new Carre1().aire1());
 		System.out.println(new Carre1().aire2());
+	
+		Forme[] tab1 = new Forme[3];
+			//	tab1[0] = new Cercle();
+			//	tab1[1] = new Triangle();
+			//	tab1[2] = new Carre1();
+		
+		Forme forme1 = new Cercle();
+		Forme forme2 = new Triangle();
+		Forme forme3 = new Carre1();
+		
+		tab1[0] = forme1;
+		tab1[1] = forme2;
+		tab1[2] = forme3;
+		
+		for(Forme f: tab1){
+			System.out.println(f.aire1());
+			
+		}
+    }
 	}
 
-}
